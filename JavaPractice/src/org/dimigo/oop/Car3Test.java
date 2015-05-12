93e6357a24991bc75f9eca04ec114bb75fc104da
승진 <@ParkSeungJin>
2015-05-12 15:51:01 +0900

    점수출력

--- JavaPractice/src/org/dimigo/oop/Car3Test.java
+++ JavaPractice/src/org/dimigo/oop/Car3Test.java
@@ -9,6 +9,7 @@ public class Car3Test {
 		Car3 car1 = new Car3("현대자동차","제네시스","검정색",225,50000000);
 		Car3 car2 = new Car3("기아자동차","K7","흰색",246);
 		Car3 car3 = new Car3("삼성자동차","SM7","회색");
+		Car3 car4 = new Car3();
 		
 		System.out.println("제조사명 : "+car1.getCompany());
 		System.out.println("모델명 : " + car1.getModel());
---
+++ JavaPractice/src/org/dimigo/oop/Score.java
@@ -0,0 +1,38 @@
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+public class Score {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("국어 점수 입력 => ");
+		int kor = scanner.nextInt();
+		System.out.println("수학 점수 입력 => ");
+		int math = scanner.nextInt();
+		System.out.println("영어 점수 입력 => ");
+		int eng = scanner.nextInt();
+		
+		int i=math+kor+eng;
+		float  avg = i/3;
+		
+		System.out.println("<<점수 출력>>");
+		StringBuilder sb = new StringBuilder();
+		sb.append("국어 점수 : ")
+				.append(kor + "점\n");
+		sb.append("수학 점수 : ")
+				.append(math + "점\n");
+		sb.append("영어 점수 : ")
+				.append(eng + "점\n");
+		sb.append("총점 : ")
+				.append(i+"점\n");
+		sb.append("평균 : ")
+				.append(String.format("%.1f",avg) +"점\n");
+		
+		System.out.println(sb);
+		
+		
+
+	}
+
+}
