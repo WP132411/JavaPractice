2156736e1472ed794bdfbe3c5819537bbaaed3b1
승진 <@ParkSeungJin>
2015-05-19 15:45:04 +0900

    과자

---
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -0,0 +1,20 @@
+package org.dimigo.oop;
+
+public class SnackTest {
+
+	public static void main(String[] args) {
+		Snack[] snacks = null;
+		snacks = new Snack[3];
+		snacks[0] = new Snack("새우깡", "농심",1100, 2);
+		snacks[1] = new Snack("콘칩", "크라운",1200, 1);
+		snacks[2] = new Snack("허니버터칩", "해태",1500, 4);
+		
+		for(Snack value : snacks) {
+			value.printSnack();
+			System.out.println();
+		}
+	
+
+	}
+
+}
