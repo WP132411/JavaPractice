47bc98380dace49f257bc698bf92ad6b4fddaf46
승진 <@ParkSeungJin>
2015-08-26 09:47:05 +0900

    스마트폰

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+public class SmartPhone {
+	
+	private String model;
+	private String company;
+	private int price;
+	
+	public SmartPhone() {
+		
+	}
+	
+	public SmartPhone(String model, String company, int price) {
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	
+	public void turnOn() {
+		System.out.println(model + "의 전원을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println(model + "의 전원을 끕니다.");
+	}
+	
+	public void pay() {
+		System.out.println(company + " 페이로 결제합니다.");
+	}
+	
+	public void useSpecialFunction() {
+		if(this instanceof Galaxy) {
+			((Galaxy) this).useWirelessCharging();
+		} else if(this instanceof IPhone) {
+			((IPhone) this).useAirDrop();
+		}
+	}
+	
+	public String toString() {
+		return "모델명 : " + this.model +", 제조사 : " + this.company + ", 가격 : " + String.format("%,d",price) + "원";
+	}
+
+}
